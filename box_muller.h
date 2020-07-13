1781f52cccbdfcfc0fb105aa8983287b50c5ff85
Makoto <ookunimasaharu@gmail.com>
2020-07-13 22:21:21 +0900

    rewrite flag

--- box_muller.h
+++ box_muller.h
@@ -3,6 +3,8 @@
 
 #include<random>
 #include<cmath>
+#define sin_flag 0b0001
+#define cos_flag 0b0010
 
 std::random_device seed_gen;
 std::default_random_engine engine(seed_gen());
@@ -13,8 +15,8 @@ inline double get_rand(void){
 	return dist(engine);
 }
 
-inline double box_mullar(double x, double y, char flag){
-	if(flag == 's') return sqrt(-2 * log(x)) * sin(2 * M_PI * y);
-	else if(flag == 'c') return sqrt(-2 * log(x)) * cos(2 * M_PI * y);
+inline double box_mullar(double x, double y, int flag){
+	if(sin_flag & flag) return sqrt(-2 * log(x)) * sin(2 * M_PI * y);
+	else if(cos_flag & flag) return sqrt(-2 * log(x)) * cos(2 * M_PI * y);
 }
 #endif
